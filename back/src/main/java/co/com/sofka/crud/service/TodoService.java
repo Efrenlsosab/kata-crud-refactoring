57b4bd289ef8680d50555bebb5d6ac0983d507b8
Efren leonarso sosa <efrenl.sosab@gmail.com>
2021-06-03 16:54:31 -0500

    se intrega la lista y save al DTO

--- back/src/main/java/co/com/sofka/crud/service/TodoService.java
+++ back/src/main/java/co/com/sofka/crud/service/TodoService.java
@@ -1,6 +1,8 @@
 package co.com.sofka.crud.service;
 
+import co.com.sofka.crud.dto.TodoDTO;
 import co.com.sofka.crud.entity.Todo;
+import co.com.sofka.crud.mapper.TodoMapper;
 import co.com.sofka.crud.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,13 +12,16 @@ public class TodoService {
 
     @Autowired
     private TodoRepository repository;
-
-    public Iterable<Todo> list(){
-        return repository.findAll();
+    @Autowired
+private TodoMapper mapper;
+    public Iterable<TodoDTO> list(){
+Iterable<Todo> todos = repository.findAll();//se trae un iterable 
+        return mapper.toTodoDTOs(todos);
     }
 
-    public Todo save(Todo todo){
-        return repository.save(todo);
+    public TodoDTO save(TodoDTO todoDTO){
+        Todo todo = mapper.aTodo(todoDTO);
+        return mapper.convertiraDTO(repository.save(todo));
     }
 
     public void delete(Long id){
